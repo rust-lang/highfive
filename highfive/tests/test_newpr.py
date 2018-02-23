f743b3ace146ce5da886b9eddc602ca9110552cd
David Alber <alber.david@gmail.com>
2018-02-23 01:05:02 -0800

    Testing behavior when global config contains already-defined groups

--- highfive/tests/test_newpr.py
+++ highfive/tests/test_newpr.py
@@ -49,7 +49,10 @@ class TestChooseReviewer(TestNewPR):
                 "groups": {
                     "core": ["@alexcrichton"],
                 }
-            }
+            },
+            'has_all': {
+                "groups": { "all": ["@alexcrichton"] }
+            },
         }
 
     def choose_reviewer(
@@ -164,6 +167,18 @@ class TestChooseReviewer(TestNewPR):
         )
         self.assertEqual(set(['alexcrichton']), chosen_reviewers)
         self.assertEqual(set([()]), mentions)
+
+    @mock.patch('highfive.newpr._load_json_file')
+    def test_global_group_overlap(self, mock_load_json):
+        """Test for an AssertionError when the global config contains a group
+        already defined in the config.
+        """
+        mock_load_json.return_value = self.global_['has_all']
+        self.assertRaises(
+            AssertionError, newpr.choose_reviewer, 'rust', 'rust-lang',
+            self.diff['normal'], 'fooauthor',
+            self.config['individuals_no_dirs']
+        )
 
     def test_no_potential_reviewers(self):
         """Test choosing a reviewer when nobody qualifies.
