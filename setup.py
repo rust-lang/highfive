e21037b25fd31ce8d9eeb8d86c9d3c8270b9ac1d
Pietro Albini <pietro@pietroalbini.org>
2019-03-15 09:00:46 +0100

    switch to setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='highfive',
-      version='0.1',
-      description='GitHub hooks to provide an encouraging atmosphere for new contributors',
-      author='Rust Community',
-      author_email='no-idea@no-server.no-suffix',
-      url='https://github.com/rust-lang-nursery/highfive',
-      packages=['highfive'],
+setup(
+    name = 'highfive',
+    version = '0.1',
+    description = 'GitHub hooks to provide an encouraging atmosphere for new contributors',
+    author = 'Rust Community',
+    author_email = 'no-idea@no-server.no-suffix',
+    url = 'https://github.com/rust-lang-nursery/highfive',
+    packages = [
+        'highfive',
+    ],
 )
