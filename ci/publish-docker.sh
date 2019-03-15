150e59423d0941bce68f4ad504c40cadab8f48aa
Pietro Albini <pietro@pietroalbini.org>
2019-03-15 15:53:40 +0100

    automatically build and push the image to ecr

---
+++ ci/publish-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ECR_IMAGE="890664054962.dkr.ecr.us-west-1.amazonaws.com/rust-highfive:latest"
+
+$(aws ecr get-login --no-include-email --region us-west-1)
+
+docker build -t rust-highfive .
+docker tag rust-highfive:latest "${ECR_IMAGE}"
+docker push "${ECR_IMAGE}"
