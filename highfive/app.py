a890a07e8da2efbb89488a064abe6100bccff2d9
Pietro Albini <pietro@pietroalbini.org>
2019-05-04 12:26:31 +0200

    increase information contained in error logs

--- highfive/app.py
+++ highfive/app.py
@@ -1,7 +1,11 @@
+from __future__ import print_function
+
+import datetime
 import hashlib
 import hmac
 import json
 import sys
+import traceback
 
 from .config import Config, InvalidTokenException
 from .newpr import HighfiveHandler, UnsupportedRepoError
@@ -24,15 +28,19 @@ def create_app(config, webhook_secret=None, config_dir=None):
     def new_pr():
         raw_data = flask.request.get_data()
 
+        # Load all the headers
+        try:
+            event = str(flask.request.headers['X-GitHub-Event'])
+            delivery = str(flask.request.headers['X-GitHub-Delivery'])
+            signature = str(flask.request.headers['X-Hub-Signature'])
+        except KeyError:
+            return 'Error: some required webhook headers are missing\n', 400
+
         # Check the signature only if the secret is configured
         if 'payload' in flask.request.form and webhook_secret is not None:
             expected = hmac.new(str(webhook_secret), digestmod=hashlib.sha1)
             expected.update(raw_data)
             expected = expected.hexdigest()
-            try:
-                signature = str(flask.request.headers['X-Hub-Signature'])
-            except KeyError:
-                return 'Error: missing signature\n', 400
             if not hmac.compare_digest('sha1='+expected, signature):
                 return 'Error: invalid signature\n', 403
 
@@ -42,9 +50,18 @@ def create_app(config, webhook_secret=None, config_dir=None):
             return 'Error: missing or invalid payload\n', 400
         try:
             handler = HighfiveHandler(Payload(payload), config, config_dir)
-            return handler.run(flask.request.headers['X-GitHub-Event'])
+            return handler.run(event)
         except UnsupportedRepoError:
             return 'Error: this repository is not configured!\n', 400
+        except:
+            print()
+            print('An exception occured while processing a webhook!')
+            print('Time:', datetime.datetime.now())
+            print('Delivery ID:', delivery)
+            print('Event name:', event)
+            print('Payload:', json.dumps(payload))
+            print(traceback.format_exc())
+            return 'Internal server error\n', 500
 
     @app.route('/')
     def index():
@@ -62,9 +79,9 @@ def cli(port, github_token, webhook_secret, config_dir):
     try:
         config = Config(github_token)
     except InvalidTokenException:
-        print 'error: invalid github token provided!'
+        print('error: invalid github token provided!')
         sys.exit(1)
-    print 'Found a valid GitHub token for user @' + config.github_username
+    print('Found a valid GitHub token for user @' + config.github_username)
 
     app = create_app(config, webhook_secret, config_dir)
     waitress.serve(app, port=port)
