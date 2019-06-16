fc38bf20773ca5765c93079199fece1649c03436
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-06-16 11:17:19 +0200

    Port highfive to python3

--- highfive/tests/patcherize.py
+++ highfive/tests/patcherize.py
@@ -9,9 +9,9 @@ def patcherize():
     def _patcherize(patcher_names=None):
         for n,p in (patcher_names or ()):
             patchers[n] = mock.patch(p)
-        return {n: p.start() for n,p in patchers.iteritems()}
+        return {n: p.start() for n,p in patchers.items()}
 
     yield _patcherize
 
-    for patcher in patchers.itervalues():
+    for patcher in patchers.values():
         patcher.stop()
